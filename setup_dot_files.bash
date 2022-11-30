a570a7a28e6d6f6d9e1b35dae258cb698f47ca00
raw_t <frevo93@gmail.com>
2022-11-30 11:21:54 +0100

    Fix zsh

--- setup_dot_files.bash
+++ setup_dot_files.bash
@@ -5,26 +5,26 @@ sudo apt update
 # install necessary packages for plugins
 sudo apt install -y neovim clang-format clang-tidy clangd flake8 python3-autopep8 tmux silversearcher-ag ripgrep fonts-powerline zathura xclip git build-essential kitty zsh python3-pip
 sudo apt remove vim
-cd ~/ && wget https://github.com/sharkdp/bat/releases/download/v0.20.0/bat_0.20.0_amd64.deb && sudo dpkg -i bat_0.20.0_amd64.deb
+cd ~/ && wget https://github.com/sharkdp/bat/releases/download/v0.20.0/bat_0.20.0_amd64.deb && sudo dpkg -i bat_0.20.0_amd64.deb && rm bat_0*
 pip install black
 curl -sL install-node.vercel.app/lts | sudo bash
-# install keyd remapping
+# # install keyd remapping
 mkdir tools && cd tools && git clone https://github.com/rvaiya/keyd
 cd keyd && make && sudo make install 
 sudo systemctl enable keyd && sudo systemctl start keyd
 sudo cp ${ROOT_DIR}/utils/default.conf /etc/keyd/default.conf
 sudo keyd reload
-# Copy tmux vim and kitti config
+# # Copy tmux vim and kitti config
 cd 
 ln -s ${ROOT_DIR}/.vim .
 ln -s ${ROOT_DIR}/tmux/.tmux.conf .
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-# Kitty config
+# # Kitty config
 cd ~/.config && ln -s ${ROOT_DIR}/kitty .
 sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator `which kitty` 50
 sudo update-alternatives --config x-terminal-emulator
 gsettings set org.gnome.desktop.default-applications.terminal exec 'kitty'
-# neovim and plugins
+# # neovim and plugins
 mkdir -p ~/.config/nvim
 cd ~/.config/nvim
 ln -s ~/.vim/init.vim .
@@ -38,6 +38,7 @@ cd ~/.ipython/profile_default/startup && ln -s ${ROOT_DIR}/utils/python_utils/st
 cd ~/
 ln -s ${ROOT_DIR}/utils/.tmuxinator.yml
 ln -s ${ROOT_DIR}/.zshrc .
+zsh
 RUNZSH=no KEEP_ZSHRC=yes sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/plugins/zsh-syntax-highlighting
