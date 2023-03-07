813e8871d3135b6e31d610176dcc8154f1e36b1c
tizianoGuadagnino <frevo93@gmail.com>
2023-03-07 17:52:17 +0100

    Out of tree installation

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt install software-properties-common git curl wget sudo make python3-pip -y
+git clone https://github.com/tizianoGuadagnino/dotfiles.git $HOME/dotfiles
+/bin/bash $HOME/dotfiles/setup_dot_files.bash
