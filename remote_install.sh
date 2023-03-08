0066d270a0ecd6c501ad4c6efebd015481e01b83
tizianoGuadagnino <frevo93@gmail.com>
2023-03-08 11:24:02 +0100

    Consistency

--- remote_install.sh
+++ remote_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt install software-properties-common git wget make python3-pip -y
 git clone https://github.com/tizianoGuadagnino/dotfiles.git $HOME/dotfiles
-/bin/bash $HOME/dotfiles/setup_dot_files.bash
+/bin/bash $HOME/dotfiles/install.sh
