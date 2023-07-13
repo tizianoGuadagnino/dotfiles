24e4be223e73ef7e218d8604b8fe6b102acf9e0e
tizianoGuadagnino <frevo93@gmail.com>
2023-07-13 16:01:57 +0200

    Nicer...still horrible though

--- docker_test/test.sh
+++ docker_test/test.sh
@@ -3,5 +3,6 @@ if [ $(git diff | wc -l) -gt 0 ]; then
     echo "You forgot to push...test it is useless... bye bye"
     exit 666
 fi
-docker build -f Dockerfile -t dotfiles $(dirname $0)
+FOLDER=$(dirname $0)
+docker build -f ${FOLDER}/Dockerfile -t dotfiles ${FOLDER}
 docker run --rm -it dotfiles:latest sh -c "$(curl -fsSL https://raw.githubusercontent.com/tizianoGuadagnino/dotfiles/master/remote_install.sh)" && zsh
