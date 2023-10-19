51805204d2c9d4c2413466d5c7771c1da22dd5d6
tizianoGuadagnino <frevo93@gmail.com>
2023-10-19 11:45:30 +0200

    small thing

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt update
 sudo apt install -y neovim clang-format clang-tidy clangd flake8 python3-autopep8 tmux silversearcher-ag ripgrep fonts-powerline zathura xclip bat git build-essential kitty zsh python3-pip tmuxinator
 sudo apt remove vim
 pip install black cmake
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - && sudo apt-get install -y nodejs
+curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - && sudo apt-get install -y nodejs
 # # install keyd remapping
 mkdir tools && cd tools && git clone https://github.com/rvaiya/keyd
 cd keyd && make && sudo make install 
