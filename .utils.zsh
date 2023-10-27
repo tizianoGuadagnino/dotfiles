f6206eb476196043d943d8232b459393e822bddf
frevo <frevo@pop-os.localdomain>
2023-10-27 12:17:34 +0200

    gitignore macro actually works

--- .utils.zsh
+++ .utils.zsh
@@ -1,5 +1,10 @@
 function submodule_for_each() {git submodule foreach 'git $@'}
-function gitignore() { curl -sLw n https://www.toptal.com/developers/gitignore/api/$@ ;}
+function gitignore() { 
+  for language in "$@"
+  do
+    curl -sLw n https://www.toptal.com/developers/gitignore/api/"$language" >> .gitignore
+  done
+}
 function dclone() {rsync -azrvuh --progress tiziano@dataserver:"$1" .}
 function clines() {cat $1 | wc -l}
 function cfiles() {ls | wc -l}
